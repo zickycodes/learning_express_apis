9ed297cc9676c0474613ed71f553f8a6f43b0991
zickycoodes <ezekieluduak2@gmail.com>
2023-01-10 09:49:31 +0100

    Updated package.json

--- util/database.js
+++ util/database.js
@@ -1,9 +1,14 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize("learning_node_restAPIS", "root", "zicky", {
-  dialect: "mysql",
-  port: "3306",
-  host: "127.0.0.1",
-});
+const sequelize = new Sequelize(
+  "freedb_learning_node_restAPIS",
+  "freedb_Zicky",
+  "Ks3ZVqQ@EYP&GgJ",
+  {
+    dialect: "mysql",
+    port: "3306",
+    host: "sql.freedb.tech",
+  }
+);
 
 module.exports = sequelize;
